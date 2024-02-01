8da6151ac68d2ced79a384aa55f0fb86a789ceb3
Maksym Kucherov <maximus.the0@gmail.com>
2024-02-01 12:32:37 +0200

    Update second regex.

--- GetVer.ps1
+++ GetVer.ps1
@@ -1,7 +1,7 @@
 $resp = Invoke-WebRequest -URI https://www.apachelounge.com/download/VS17/ -UserAgent "CMake"
 If ($resp.Content -match "httpd-2\.4\.(\d+)(-\d+)?-win64-VS17\.zip")
 {
-  $ver = $Matches[0] -replace "^.*httpd-2.4.(\d+).*", '$1'
+  $ver = $Matches[0] -replace "^.*httpd-2.4.(\d+(-\d+)?).*", '$1'
 }
 Else
 {
